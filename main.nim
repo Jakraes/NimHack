0413b056b7908b57c0c9dc06e9db5ac5bb8e42cb
Jakraes <70046168+Jakraes@users.noreply.github.com>
2022-03-20 03:32:28 +0000

    Add files via upload

--- main.nim
+++ main.nim
@@ -5,15 +5,15 @@ import random
 randomize()
 
 const
-    tH = 11
-    tW = 24
-    windowSize = 9
+    tH = 11 # Terminal Height (Actually the height of the menu.txt file)
+    tW = 24 # Terminal Width (Actually the width of the menu.txt file)
+    windowSize = 9 # Size of the window where the map will be drawn on
 var
-    menuReset: array[tH, array[tW, char]]
-    menu = menuReset
+    menuReset: array[tH, array[tW, char]] # Array used to assign the menu back to its original state
+    menu = menuReset # Actual menu
     temp: int
-
-for line in lines("menu.txt"):
+ 
+for line in lines("menu.txt"): # Building the menuReset array from the menu.txt file
     for i in 0..<tW:
         menuReset[temp][i] = line[i]
     temp += 1
@@ -21,71 +21,71 @@ for line in lines("menu.txt"):
 #-----------------------------------------------------------#
 
 var 
-    world = generateWorld()
-    camPos, playerPos, previousPos: tuple[x, y: int]
-    running = true
-    hp, mp = 10
-    floorCoordinates: seq[tuple[x,y:int]]
-    steps = 0
+    world = generateWorld() # World array
+    camPos, playerPos, previousPos: tuple[x, y: int] # Self explanatory
+    running = true # Game running
+    hp, mp = 10 # Self explanatory
+    floorCoordinates: seq[tuple[x,y:int]] # Used to choose a spawn point
+    steps = 0 # Self explanatory
 
-for y in 0..<world.len():
+for y in 0..<world.len(): # Building the floorCoordinates
     for x in 0..<world.len():
         if world[y][x] == '.':
             floorCoordinates.add((x,y))
 
-playerPos = floorCoordinates[rand(floorCoordinates.len()-1)]
-previousPos = playerPos
-camPos = (playerPos.x-3, playerPos.y-3)
+playerPos = floorCoordinates[rand(floorCoordinates.len()-1)] # Chooses spawn point
+previousPos = playerPos 
+camPos = (playerPos.x-int(windowSize/2), playerPos.y-int(windowSize/2))
 
-proc resetMenu() =
+proc resetMenu() = # Resets the menu back to its original state
     menu = menuReset
 
-proc drawScreen() = 
-    camPos = (playerPos.x-int(windowSize/2), playerPos.y-int(windowSize/2))
-    if camPos.x < 0:
-        camPos = (0,camPos.y)
-    if camPos.y < 0:
-        camPos = (camPos.x,0)
-    if camPos.x > world.len()-windowSize:
-        camPos = (world.len()-windowSize,camPos.y)
-    if camPos.y > world.len()-windowSize:
-        camPos = (camPos.x,world.len()-windowSize)
+proc drawScreen() = # Draws to the terminal
+    camPos = (playerPos.x-int(windowSize/2), playerPos.y-int(windowSize/2)) # Top left corner of the game window 
+    if camPos.x < 0:                                #
+        camPos = (0,camPos.y)                       #
+    if camPos.y < 0:                                #
+        camPos = (camPos.x,0)                       #
+    if camPos.x > world.len()-windowSize:           # > Basically ensures that the camera isn't outside of the map when it follows the player
+        camPos = (world.len()-windowSize,camPos.y)  #
+    if camPos.y > world.len()-windowSize:           #
+        camPos = (camPos.x,world.len()-windowSize)  #
     var 
-        output: string
+        output: string # Output string of the whole function
         hps = $hp
         mps = $mp
         stepss = $steps
-    if hps.len() == 1:
+    if hps.len() == 1: # Decides if it needs to add a 0 to a single digit number
         hps = "0" & hps
-    if mps.len() == 1:
+    if mps.len() == 1: # Same as above
         mps = "0" & mps
-    for i in 0..<(6-stepss.len()):
+    for i in 0..<(6-stepss.len()): # Same as above pretty much
         stepss = "0" & stepss
-    menu[1][tW-10] = hps[0]
-    menu[1][tW-9] = hps[1]
-    menu[1][tW-3] = mps[0]
-    menu[1][tW-2] = mps[1]
-    for i in 1..6:
+    menu[1][tW-10] = hps[0] # 
+    menu[1][tW-9] = hps[1]  # > Draws the HP and MP values to the menu array
+    menu[1][tW-3] = mps[0]  #
+    menu[1][tW-2] = mps[1]  #
+    for i in 1..6: # Draws the step value
         menu[3][tW-i-1] = stepss[6-i]
-    for y in 0..<windowSize:
+    for y in 0..<windowSize: # Draws the local map to the menu
         for x in 0..<windowSize:
             menu[y+1][x+1] = world[camPos.y+y][camPos.x+x]
-    menu[int(windowSize/2)+1+(playerPos.y - camPos.y - int(windowSize/2))][int(windowSize/2)+1+(playerPos.x - camPos.x - int(windowSize/2))] = '@'
-    eraseScreen()
+    menu[int(windowSize/2)+1+(playerPos.y - camPos.y - int(windowSize/2))][int(windowSize/2)+1+(playerPos.x - camPos.x - int(windowSize/2))] = '@' # Draws the player to the menu
+    eraseScreen() # Clears the screen 
     setCursorPos(0,0)
     echo """+----------------------+
 |       ~NimHack~      |"""
-    for y in 0..<tH:
+    for y in 0..<tH: # Writes to output
         for x in 0..<tW:
             output = output & menu[y][x]
         output = output & '\n'
-    echo output
+    echo output # Outputs
 
-proc collision() =
+proc collision() = # Checks collision between the player and map tiles
     if world[playerPos.y][playerPos.x] == '#':
         playerPos = previousPos
 
-proc getInput() =
+proc getInput() = # Self explanatory
     let input = getch()
     previousPos = playerPos
     case input
@@ -125,7 +125,7 @@ echo """
 |       or Q to quit      |
 +-------------------------+
 """
-while running:
+while running: # Game loop
     resetMenu()
     getInput()
     collision()
